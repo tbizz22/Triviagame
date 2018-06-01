2d37558e8b76fb8fe85273c648967528e51744c3
tyler bisbee <tyler.bisbee@gmail.com>
2018-05-31 23:56:05 -0400

    added gifs for question responses

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -15,8 +15,49 @@ function countdown() {
 
 
 
+//Object
+var question = {
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    question1: {
+        question:"What is the next question",
+        answer1:"1",
+        answer2:"2",
+        answer3:"3",
+        answer4:"4"
+    },
+    question2: {
+        question:"This is the next question",
+        answer1:"5",
+        answer2:"6",
+        answer3:"7",
+        answer4:"8"
+    },
+    question3: {
+        question:"",
+        answer1:"",
+        answer2:"",
+        answer3:"",
+        answer4:""
+    },
+    question4: {
+        question:"",
+        answer1:"",
+        answer2:"",
+        answer3:"",
+        answer4:""
+    },
+    question5: {
+        question:"",
+        answer1:"",
+        answer2:"",
+        answer3:"",
+        answer4:""
+    },
+answerClicked:null
 
-var question = 
+}
 
 
 

5a0dbfc42dc4876d0444b4bf7e12c70db4553ba6
tyler bisbee <tyler.bisbee@gmail.com>
2018-05-31 23:41:53 -0400

    added start screen button

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -15,4 +15,35 @@ function countdown() {
 
 
 
-countdown();
+
+var question = 
+
+
+
+// Start the game
+$("#intro").on ("click","#begin", function() {
+    $("#intro").addClass("hide");
+    $("#questionGrid").removeClass("hide");
+    countdown();
+});
+
+// Which button is clicked?
+//answer 1
+$("#answers").on("click","#answer1",function() {
+    alert("answer1");
+});
+
+//answer 2
+$("#answers").on("click","#answer2",function() {
+    alert("answer2");
+});
+
+//answer 3
+$("#answers").on("click","#answer3",function() {
+    alert("answer3");
+});
+
+//answer 4
+$("#answers").on("click","#answer4",function() {
+    alert("answer4");
+});

6b3dd27aea82aabc9dac6b3aa7b85ee4f5fcc9c4
Tyler Bisbee <Tyler.Bisbee@gmail.com>
2018-06-01 22:15:57 -0400

    fixed timer function

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,66 +1,69 @@
 //timer function
+function timer() {
+    var timeLeft = 9;
+    var timerId = setInterval(countdown, 1000);
 
-var timeLeft = 30;
-var timerId = setInterval(countdown, 1000);
-
-function countdown() {
-    if (timeLeft === 0) {
-        clearTimeout(timerId);
-        //put code here to end the round
-    } else {
-        $("#timer").html(timeLeft);
-        timeLeft--
-    };
+    function countdown() {
+        if (timeLeft === -1) {
+            clearTimeout(timerId);
+            //put code here to end the round
+        } else {
+            $("#timer").html(timeLeft);
+            timeLeft--
+        };
+    }
 }
 
 
 
 //Object
-var question = {
+var game = {
     correct: 0,
     incorrect: 0,
     unanswered: 0,
-    question1: {
-        question:"What is the next question",
-        answer1:"1",
-        answer2:"2",
-        answer3:"3",
-        answer4:"4",
-        rightAnswer:""
+    questions: {
+        question1: {
+            question: "What is the next question",
+            answer1: "1",
+            answer2: "2",
+            answer3: "3",
+            answer4: "4",
+            rightAnswer: ""
+        },
+        question2: {
+            question: "This is the next question",
+            answer1: "5",
+            answer2: "6",
+            answer3: "7",
+            answer4: "8",
+            rightAnswer: ""
+        },
+        question3: {
+            question: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            rightAnswer: ""
+        },
+        question4: {
+            question: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            rightAnswer: ""
+        },
+        question5: {
+            question: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            rightAnswer: ""
+        }
     },
-    question2: {
-        question:"This is the next question",
-        answer1:"5",
-        answer2:"6",
-        answer3:"7",
-        answer4:"8",
-        rightAnswer:""
-    },
-    question3: {
-        question:"",
-        answer1:"",
-        answer2:"",
-        answer3:"",
-        answer4:"",
-        rightAnswer:""
-    },
-    question4: {
-        question:"",
-        answer1:"",
-        answer2:"",
-        answer3:"",
-        answer4:"",
-        rightAnswer:""
-    },
-    question5: {
-        question:"",
-        answer1:"",
-        answer2:"",
-        answer3:"",
-        answer4:"",
-        rightAnswer:""
-    },
-answerClicked:null
+    answerClicked: null
 
 }
 
@@ -70,7 +73,7 @@ answerClicked:null
 $("#intro").on ("click","#begin", function() {
     $("#intro").addClass("hide");
     $("#questionGrid").removeClass("hide");
-    countdown();
+    timer();
 });
 
 // Which button is clicked?

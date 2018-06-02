c9014ec9d5caf174d049bf6fc7a484964ad5ab6d
tyler bisbee <tyler.bisbee@gmail.com>
2018-06-02 00:31:53 -0400

    working for single question

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -14,7 +14,7 @@ function timer() {
     }
 }
 
-
+var uxdisplay = null;
 
 //Object
 var game = {
@@ -24,11 +24,22 @@ var game = {
     questions: {
         question1: {
             question: "What is the next question",
-            answer1: "1",
-            answer2: "2",
-            answer3: "3",
-            answer4: "4",
-            rightAnswer: ""
+            answer1: {
+                value: "1",
+                correct: false
+            },                
+            answer2: {
+                value: "2",
+                correct: false
+            },
+            answer3: {
+                value: "3",
+                correct: false
+            },
+            answer4: {
+                value: "4",
+                correct: true
+            },
         },
         question2: {
             question: "This is the next question",
@@ -63,8 +74,53 @@ var game = {
             rightAnswer: ""
         }
     },
-    answerClicked: null
+    answerClicked: null,
+    currentQuestion: null,
+}
 
+var images = {
+    win: ["office1.gif","office3.gif","office4.gif","office7.gif"],
+    lose: ["office10.gif","office9.gif","office8.gif","office6.gif","office5.gif"]
+};
+
+
+
+
+
+
+// // get the question
+// function getQuestion() {
+//     Object.keys(game.questions).forEach((key) => {
+//         game.questions[key] = game.currentQuestion;
+//         console.log(game.questions.key);
+//     });
+// };
+
+
+// function getQuestion() {
+//     Object.keys(game.questions).forEach((key) => {
+//         game.questions.currentQuestion = uxdisplay
+//     })
+
+// }
+
+// function getQuestion() {
+// for (var k in game.questions) {
+//     for (var q in k)
+//         $("#question").html(k[q]);
+
+//     console.log(k & "  " & game.questions[k]);
+// }
+// }
+
+function drawQuestion(qnum) {
+    $("#question").html(qnum.question)
+    $("#answer1").html(qnum.answer1.value);
+    $("#answer2").html(qnum.answer2.value);
+    $("#answer3").html(qnum.answer3.value);
+    $("#answer4").html(qnum.answer4.value);
+    game.currentQuestion = qnum;
+     
 }
 
 
@@ -74,26 +130,73 @@ $("#intro").on ("click","#begin", function() {
     $("#intro").addClass("hide");
     $("#questionGrid").removeClass("hide");
     timer();
+    drawQuestion(game.questions.question1);
 });
+
+
 
 // Which button is clicked?
 //answer 1
 $("#answers").on("click","#answer1",function() {
-    alert("answer1");
-    
+    game.answerClicked = game.currentQuestion.answer1
+    console.log(game.answerClicked)
+    gameLogic();
 });
 
 //answer 2
 $("#answers").on("click","#answer2",function() {
-    alert("answer2");
+    game.answerClicked = game.currentQuestion.answer2
+    gameLogic();
 });
 
 //answer 3
 $("#answers").on("click","#answer3",function() {
-    alert("answer3");
+    game.answerClicked = game.currentQuestion.answer3
+    gameLogic();
 });
 
 //answer 4
 $("#answers").on("click","#answer4",function() {
-    alert("answer4");
+    game.answerClicked = game.currentQuestion.answer4
+    gameLogic();
 });
+
+
+
+
+
+function gameLogic() {
+    if (game.answerClicked.correct === true) {
+        game.correct++
+        showResult(true);
+    } else {
+        game.incorrect++
+        showResult(false);
+    }
+};
+
+
+
+// get img 
+function getImg(array) {
+    var path = "Assets/images/"
+    var img = array[(Math.floor(Math.random()*array.length))];
+    gif = path + img;
+    return gif
+}
+
+
+// display result page
+var gif 
+var array
+function showResult(bool) {
+    if (bool === true) {
+        array = images.win
+    } else {
+        array = images.lose
+    }
+    getImg(array);    
+    $("#qResult").attr("src",gif);
+    $("#questionGrid").addClass("hide");
+    $("#resultDiv").removeClass("hide");
+}
